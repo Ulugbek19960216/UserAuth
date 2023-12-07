8a1dbf0b462dad06dfd89df5cb088f94eac9dff1
Ulugbek_Leo <sherovulugbek04@gmail.com>
2023-12-07 10:14:52 +0500

    added three routes, Home, Login, and Register

--- app.js
+++ app.js
@@ -14,6 +14,19 @@ app.use(bodyParser.urlencoded({
     extended:true
 }))
 
+app.get("/", (req, res) => {
+    res.send("home");
+})
+
+
+app.get("/login", (req, res) => {
+    res.send("login");
+})
+
+
+app.get("/register", (req, res) => {
+    res.send("register");
+})
 
 
 
