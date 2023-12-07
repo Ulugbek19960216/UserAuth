41df3380e34352f14ae59e42aba8dd8f7c74843d
Ulugbek_Leo <sherovulugbek04@gmail.com>
2023-12-08 00:04:48 +0500

    User registration and login successfully connect to database and stored user data

--- app.js
+++ app.js
@@ -2,19 +2,82 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const path = require("path");
+const mongoose = require("mongoose");
 
 const app = express();
 const port = 3000;
 
-app.set("views", path.join(__dirname, "server", "views"));
+const templatePath = path.join(__dirname, "./server/views");
 
 
-app.use(express.static(path.join(__dirname, "public")));
-app.set("view engine", 'ejs');
-
+app.use(express.static("public"));
 app.use(bodyParser.urlencoded({
-    extended:true
+    extended:false
 }));
+
+app.set("view engine", 'ejs');
+app.set("views", templatePath);
+// Connect to the MongoDB database using the specified URL
+mongoose
+  .connect("mongodb://127.0.0.1:27017/userDB")
+  .then(() => {
+    console.log('Mongoose connected successfully');
+  })
+  .catch((error) => {
+    console.error('Failed to connect to MongoDB:', error);
+  });
+
+
+const userSchema = {
+    firstName: String,
+    lastName: String,
+    address: String,
+    phoneNumber: String,
+    email: String,
+    password: String
+};
+ 
+// 
+const User = new mongoose.model("User", userSchema);
+
+// user registration, saving password and name on database
+app.post("/register", function(req, res) {
+    const newUser = new User({
+        firstName: req.body.firstName,
+        lastName:   req.body.lastName,
+        address:    req.body.address,
+        phoneNumber: req.body.phone,
+        email: req.body.email,
+        password: req.body.password
+    });
+
+    newUser.save() 
+        .then (() => {
+            res.render("home")
+        }) 
+        .catch(err =>  {
+            console.log(err);
+        })
+    
+});
+
+app.post("/login", (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    User.findOne({email: email})
+        .then((foundUser) => {
+            if(foundUser && foundUser.password === password) {
+                res.render("home");
+                console.log("successfully logged in");
+            }
+        })
+        .catch((err) => {
+            res.render("login", { error: "Incorrect password" });
+        });
+})
+
+
 
 app.get('/home', (req, res) => {
     res.render("home");
